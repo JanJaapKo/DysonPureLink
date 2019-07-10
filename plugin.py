6d42a06ed77ee36577e9ef48833b4f53ab8f91bd
JanJaapKo <44156144+JanJaapKo@users.noreply.github.com>
2019-07-10 16:39:15 +0200

    add 527

--- plugin.py
+++ plugin.py
@@ -3,7 +3,7 @@
 # Author: Jan-Jaap Kostelijk
 #
 """
-<plugin key="DysonPureLink" name="Dyson Pure Link" author="Jan-Jaap Kostelijk" version="1.0.2" wikilink="https://github.com/JanJaapKo/DysonPureLink.wiki.git" externallink="https://github.com/JanJaapKo/DysonPureLink">
+<plugin key="DysonPureLink" name="Dyson Pure Link" author="Jan-Jaap Kostelijk" version="1.0.3" wikilink="https://github.com/JanJaapKo/DysonPureLink.wiki.git" externallink="https://github.com/JanJaapKo/DysonPureLink">
     <description>
         <h2>Dyson Pure Link plugin</h2><br/>
         Connects to Dyson Pure Link devices<br/>
@@ -19,6 +19,7 @@
                 <option label="465" value="465"/>
                 <option label="469" value="469"/>
                 <option label="475" value="475" default="true"/>
+                <option label="527" value="527"/>
             </options>
         </param>
 		<param field="Username" label="Dyson Serial No." required="true"/>
