b3d5ec476b9ede0b553e553010aeee4ec3291f33
janjaapko <janjaap.kostelijk@gmail.com>
2020-09-22 08:18:12 +0200

    add state to heatMode second try

--- value_types.py
+++ value_types.py
@@ -88,8 +88,8 @@ class HeatMode():
 
     @property
     def state(self):
-        if self._state == self.OFF: return 0
-        if self._state == self.HEAT: return 1
+        if self._state == self.OFF: return 10
+        if self._state == self.HEAT: return 20
 
 """Custom Errors"""
 class ConnectionError(Exception):
